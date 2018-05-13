a10be24975357b06e787b7cc0d3970876eae739f
nucle013 <39242306+nucle013@users.noreply.github.com>
2018-05-13 18:25:52 +0200

    Add files via upload

---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() created_at: Date;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onLoveIt() {
+    this.loveIts++;
+  }
+
+  onDontLoveIt() {
+    this.loveIts--;
+  }
+
+  getColor() {
+    if(this.loveIts > 0) {
+      return 'green';
+    } else if(this.loveIts < 0) {
+      return 'red';
+    }
+  }
+}
